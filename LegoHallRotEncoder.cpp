75ea31a693b2471599bc897d4f304cbf50777da7
Erich Eichinger <eeichinger@gmail.com>
2020-06-12 23:46:16 +0200

    move enc table to PROGMEM

--- LegoHallRotEncoder.cpp
+++ LegoHallRotEncoder.cpp
@@ -2,16 +2,36 @@
 #include "LegoHallRotEncoder.h"
 #include <Arduino.h>
 
-int8_t rot_enc_table[255] = {    0    };
+static const int8_t PROGMEM rot_enc_table[] = {
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, -1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, -1, 1, 0, 0, -1, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 0,
+0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
 
-volatile uint8_t _prevNextCode = 0;
-volatile int _counter = 0;
-volatile int8_t _dir = 0;
+
+
+static volatile uint8_t _prevNextCode = 0;
+static volatile int _counter = 0;
+static volatile int8_t _dir = 0;
 
 int PIN_HALL1 = -1;
 int PIN_HALL2 = -1;
 
-void init_encoder_table() {
+/*
+extern void init_encoder_table() {
   // dir +1
   rot_enc_table[B00011101] = 1;
   rot_enc_table[B01110111] = 1;
@@ -29,11 +49,12 @@ void init_encoder_table() {
   rot_enc_table[B01110100] = -1;
   rot_enc_table[B11010010] = -1;
   rot_enc_table[B01001011] = -1;
-  rot_enc_table[B00101110] = -1;  
+  rot_enc_table[B00101110] = -1;
 }
+*/
 
 // A vald CW or CCW move returns 1, invalid returns 0.
-int8_t read_rotary() {
+static int8_t read_rotary() {
   // experimental sequence dir=1: 00 01 11 01 11 10 11 10
 //  static int8_t rot_enc_table[255] = {    0    };
 
@@ -43,7 +64,8 @@ int8_t read_rotary() {
   if (digitalRead(PIN_HALL2)) _prevNextCode |= 0x01;
   _prevNextCode &= 0xff;
 
-  return ( rot_enc_table[( _prevNextCode )]);
+  return pgm_read_byte_near(rot_enc_table + _prevNextCode);
+  // return ( rot_enc_table[( _prevNextCode )]);
 }
 
 /*static*/ void LegoHallRotEncoder::rotenc_changed() {
@@ -56,7 +78,6 @@ LegoHallRotEncoder::LegoHallRotEncoder(uint8_t pin1, uint8_t pin2) {
   if (PIN_HALL1 != -1) {
     return;
   }
-  init_encoder_table();
   PIN_HALL1 = pin1;
   PIN_HALL2 = pin2;
   pinMode(pin1, INPUT_PULLUP);
